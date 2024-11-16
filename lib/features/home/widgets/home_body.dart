bff56d11f515e42223d818f2c78b336e5a4ccac9
Josh Birdwell <josh.birdwell@life.church>
2024-11-16 00:14:37 -0600

    Refactor HomeBody to use state management
    - Changed HomeBody from StatelessWidget to StatefulWidget.
    - Moved voiceActors list into the state class and added loading/saving functionality with SharedPreferences.
    - Updated the ListView to a ReorderableListView for drag-and-drop reordering of items.

--- lib/features/home/widgets/home_body.dart
+++ lib/features/home/widgets/home_body.dart
@@ -1,47 +1,89 @@
 import 'package:anime_convention/features/home/widgets/home_row.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-const List<String> voiceActors = [
-  "Christopher Sabat",
-  "Troy Baker",
-  "Sean Schemmel",
-  "Todd Haberkorn",
-  "Maile Flanagan",
-  "Clifford Chapin",
-  "Mallorie Rodak",
-  "Amanda Lee",
-  "Brittney Karbowski",
-  "Jamie Marchi",
-  "Monica Rial",
-  "Jason Liebrecht",
-  "Elizabeth Maxwell",
-  "Brandon McInnis",
-  "Kristen McGuire",
-  "Kimiko Glenn",
-  "J Michael Tatum",
-  "Kellen Goff",
-  "Richard Horvitz",
-  "Justin Cook",
-  "Alejandro Saab",
-  "Yuri Lowenthal",
-  "Tara Platt",
-  "Erica Lindbeck",
-  "Edward Bosco",
-  "Cristina Vee",
-  "Kate Higgins",
-  "Griffin Burns",
-];
-
-class HomeBody extends StatelessWidget {
+class HomeBody extends StatefulWidget {
   const HomeBody({super.key});
 
   @override
-  Widget build(BuildContext context) => ListView.builder(
+  HomeBodyState createState() => HomeBodyState();
+}
+
+class HomeBodyState extends State<HomeBody> {
+  List<String> voiceActors = [
+    "Christopher Sabat",
+    "Troy Baker",
+    "Sean Schemmel",
+    "Todd Haberkorn",
+    "Maile Flanagan",
+    "Clifford Chapin",
+    "Mallorie Rodak",
+    "Amanda Lee",
+    "Brittney Karbowski",
+    "Jamie Marchi",
+    "Monica Rial",
+    "Jason Liebrecht",
+    "Elizabeth Maxwell",
+    "Brandon McInnis",
+    "Kristen McGuire",
+    "Kimiko Glenn",
+    "J Michael Tatum",
+    "Kellen Goff",
+    "Richard Horvitz",
+    "Justin Cook",
+    "Alejandro Saab",
+    "Yuri Lowenthal",
+    "Tara Platt",
+    "Erica Lindbeck",
+    "Edward Bosco",
+    "Cristina Vee",
+    "Kate Higgins",
+    "Griffin Burns",
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadVoiceActors();
+  }
+
+  Future<void> _loadVoiceActors() async {
+    final prefs = await SharedPreferences.getInstance();
+    final savedList = prefs.getStringList('voiceActors');
+    if (savedList != null) {
+      setState(() {
+        voiceActors = savedList;
+      });
+    }
+  }
+
+  Future<void> _saveVoiceActors() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setStringList('voiceActors', voiceActors);
+  }
+
+  @override
+  Widget build(BuildContext context) => Padding(
         padding: const EdgeInsets.all(16),
-        itemCount: voiceActors.length,
-        itemBuilder: (context, index) {
-          final name = voiceActors[index];
-          return HomeRow(name: name);
-        },
+        child: ReorderableListView.builder(
+          itemCount: voiceActors.length,
+          onReorder: (oldIndex, newIndex) {
+            setState(() {
+              if (newIndex > oldIndex) {
+                newIndex--; // Adjust index if moving down
+              }
+              final item = voiceActors.removeAt(oldIndex);
+              voiceActors.insert(newIndex, item);
+              _saveVoiceActors(); // Save the updated order
+            });
+          },
+          itemBuilder: (context, index) {
+            final name = voiceActors[index];
+            return HomeRow(
+              key: ValueKey(name), // Each item needs a unique key
+              name: name,
+            );
+          },
+        ),
       );
 }
